b1710af6c535700123e52bab36280d184f0f5433
mathieu <mathieu.beriac@ynov.com>
2026-03-04 13:53:49 +0100

    feat: :construction: Added route and controller for recipe ingredient

---
+++ routes/routes_recipe_ingredient.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getAllRecipeIngredients,
+    getRecipeIngredientsByRecipeId,
+    getRecipeIngredientsByIngredientId,
+    addRecipeIngredient,
+    deleteRecipeIngredient,
+    updateRecipeIngredient
+} = require('../controllers/controller_recipe_ingredient');
+
+router.get('/', getAllRecipeIngredients);                        // GET /recipe-ingredients
+router.get('/recipe/:id_recipe', getRecipeIngredientsByRecipeId); // GET /recipe-ingredients/recipe/:id_recipe
+router.get('/ingredient/:id_ingredient', getRecipeIngredientsByIngredientId); // GET /recipe-ingredients/ingredient/:id_ingredient
+router.post('/', addRecipeIngredient);                                        // POST /recipe-ingredients
+router.delete('/:id_recipe/:id_ingredient', deleteRecipeIngredient);          // DELETE /recipe-ingredients/:id_recipe/:id_ingredient
+router.put('/:id_recipe/:id_ingredient', updateRecipeIngredient);             // PUT /recipe-ingredients/:id_recipe/:id_ingredient
+
+module.exports = router;
--- server.js
+++ server.js
@@ -12,11 +12,13 @@ const recipesRoutes = require('./routes/routes_recipes');
 const usersRoutes = require('./routes/routes_users');
 const favoryRoutes = require('./routes/routes_favory');
 const ingredientRoutes = require('./routes/routes_ingredient');
+const recipeIngredientRoutes = require('./routes/routes_recipe_ingredient');
 
 app.use('/users', usersRoutes);
 app.use('/recipes', recipesRoutes);
 app.use('/favory', favoryRoutes);
 app.use('/ingredient', ingredientRoutes);
+app.use('/recipe_ingredient', recipeIngredientRoutes);
 
 // Route de test
 app.get('/', (req, res) => {
@@ -29,6 +31,7 @@ app.get('/', (req, res) => {
       pictures: '/pictures',
       filters: '/filters',
       favory: '/favory',
+      recipe_indgredient: '/recipe_ingredient'
     
     }
   });
