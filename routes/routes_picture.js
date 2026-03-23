550da072ff604ded6aa726bce17f471aef5db598
mathieu <mathieu.beriac@ynov.com>
2026-03-23 11:31:12 +0100

    feat: Added gestion upload profil picture in route picture

--- routes/routes_picture.js
+++ routes/routes_picture.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const multerUploadConf = require('../uploads/upload');
 const {
   getAllPictures,
   getPictureById,
   getPicturesByType,
   addPicture,
   updatePicture,
-  deletePicture
+  deletePicture,
+  uploadProfilPicture
 } = require('../controllers/controller_picture');
 
-router.get('/', getAllPictures);              // GET /pictures
-router.get('/:id', getPictureById);           // GET /pictures/:id
-router.get('/type/:type', getPicturesByType); // GET /pictures/type/:type
-router.post('/', addPicture);                 // POST /pictures
-router.put('/:id', updatePicture);            // PUT /pictures/:id
-router.delete('/:id', deletePicture);         // DELETE /pictures/:id
+router.get('/', getAllPictures);
+router.get('/:id', getPictureById);
+router.get('/type/:type', getPicturesByType);
+router.post('/', addPicture);
+router.put('/:id', updatePicture);
+router.delete('/:id', deletePicture);
+
+// router.use('/upload/profile/:userId', (req, res) => {
+//   console.log('files:', req.files);
+//   console.log('body:', req.body);
+//   res.json({ files: req.files, body: req.body });
+// });
+
+// router.post('/upload/profile/:userId', multerUploadConf.any(), (req, res) => {
+//   console.log('files:', req.files);
+//   console.log('body:', req.body);
+//   res.json({ files: req.files, body: req.body });
+// });
+
+router.post('/upload/profile/:userId', multerUploadConf.single("avatar"), uploadProfilPicture);
 
 module.exports = router;
