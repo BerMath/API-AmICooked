e71a023fddd25e4715579276326a9e9ae829f68c
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-03-25 14:41:34 +0100

    feat: login route has been added

--- routes/routes_users.js
+++ routes/routes_users.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const {
-  getUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
 } = require('../controllers/controller_users');
+const {loginUser} = require('../controllers/controller_auth');
 
-router.get('/', getUsers);         // GET /users
-router.get('/:id', getUserById);   // GET /users/:id
-router.post('/', createUser);      // POST /users
-router.put('/:id', updateUser);    // PUT /users/:id
-router.delete('/:id', deleteUser); // DELETE /users/:id
+router.get('/', getUsers);              // GET /users
+router.get('/:id', getUserById);        // GET /users/:id
+router.post('/', createUser);           // POST /users
+router.put('/:id', updateUser);         // PUT /users/:id
+router.delete('/:id', deleteUser);      // DELETE /users/:id
+router.post('/login', loginUser);       // POST /users/login
 
 module.exports = router;
