dc4add06236e5fe05bd486727b90e74bcc98b79c
cnoah <noah.charrinbourrat@ynov.com>
2026-04-01 22:27:17 +0200

    feat: add token refresh and logout routes to user authentication

--- routes/routes_users.js
+++ routes/routes_users.js
@@ -8,7 +8,11 @@ const {
     updateUserXp,
     deleteUser
 } = require('../controllers/controller_users');
-const {loginUser} = require('../controllers/controller_auth');
+const {loginUser, refreshUserToken, logoutUser} = require('../controllers/controller_auth');
+
+router.post('/login', loginUser);       // POST /users/login
+router.post('/refresh', refreshUserToken); // POST /users/refresh
+router.post('/logout', logoutUser);     // POST /users/logout
 
 router.get('/', getUsers);              // GET /users
 router.get('/:id', getUserById);        // GET /users/:id
@@ -16,6 +20,5 @@ router.post('/', createUser);           // POST /users
 router.patch('/:id', updateUser);         // PATCH /users/:id
 router.patch('/:id/xp', updateUserXp);         // PATCH /users/:id/xp
 router.delete('/:id', deleteUser);      // DELETE /users/:id
-router.post('/login', loginUser);       // POST /users/login
 
 module.exports = router;
