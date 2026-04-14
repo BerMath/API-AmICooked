36fa1b0e0ab17a6d5c2df52e631676f8728cbd61
cnoah <noah.charrinbourrat@ynov.com>
2026-04-14 19:44:01 +0200

    feat: add routes for handling user profile pictures

--- routes/routes_users.js
+++ routes/routes_users.js
@@ -9,16 +9,20 @@ const {
     deleteUser
 } = require('../controllers/controller_users');
 const {loginUser, refreshUserToken, logoutUser} = require('../controllers/controller_auth');
+const {getProfilePicture, createProfilePicture} = require("../controllers/controller_picture");
 
-router.post('/login', loginUser);       // POST /users/login
-router.post('/refresh', refreshUserToken); // POST /users/refresh
-router.post('/logout', logoutUser);     // POST /users/logout
+router.post('/login', loginUser);               // POST /users/login
+router.post('/refresh', refreshUserToken);      // POST /users/refresh
+router.post('/logout', logoutUser);             // POST /users/logout
 
-router.get('/', getUsers);              // GET /users
-router.get('/:id', getUserById);        // GET /users/:id
-router.post('/', createUser);           // POST /users
-router.patch('/:id', updateUser);         // PATCH /users/:id
-router.patch('/:id/xp', updateUserXp);         // PATCH /users/:id/xp
-router.delete('/:id', deleteUser);      // DELETE /users/:id
+router.get('/', getUsers);                      // GET /users
+router.get('/:id', getUserById);                // GET /users/:id
+router.post('/', createUser);                   // POST /users
+router.patch('/:id', updateUser);               // PATCH /users/:id
+router.patch('/:id/xp', updateUserXp);          // PATCH /users/:id/xp
+router.delete('/:id', deleteUser);              // DELETE /users/:id
+
+router.get('/:id/picture', getProfilePicture);      // GET /users/:id/picture
+router.post('/:id/picture', createProfilePicture);  // POST /users/:id/picture
 
 module.exports = router;
