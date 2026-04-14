0c3b3565a1c4cb73685ed519787f0fc7d354d4eb
cnoah <noah.charrinbourrat@ynov.com>
2026-04-14 19:44:25 +0200

    feat: add routes for handling recipe pictures

--- routes/routes_recipes.js
+++ routes/routes_recipes.js
@@ -13,17 +13,20 @@ const {
     createRecipeFilter,
     deleteRecipeFilter
 } = require('../controllers/controller_recipe_filters');
+const {getRecipePicture, createRecipePicture} = require("../controllers/controller_picture");
 
-router.get('/', getRecipes);         // GET /recipes
-router.get('/:id', getRecipeById);   // GET /recipes/:id
-router.post('/', createRecipe);      // POST /recipes
-router.post('/:id/time', timeRecipe); // POST /recipes/:id/time
-router.put('/:id', updateRecipe);    // PUT /recipes/:id
-router.delete('/:id', deleteRecipe); // DELETE /recipes/:id
+router.get('/', getRecipes);            // GET /recipes
+router.get('/:id', getRecipeById);      // GET /recipes/:id
+router.post('/', createRecipe);         // POST /recipes
+router.post('/:id/time', timeRecipe);   // POST /recipes/:id/time
+router.put('/:id', updateRecipe);       // PUT /recipes/:id
+router.delete('/:id', deleteRecipe);    // DELETE /recipes/:id
 
-router.get('/:id/filters', getRecipeFilterByRecipeId); // GET /recipes/:id/filters
-router.post('/:id/filters', createRecipeFilter); // POST /recipes/:id/filters
-router.delete('/:id_recipe/filters/:id_filter', deleteRecipeFilter); // DELETE /recipes/:id_recipe/filters/:id_filter
+router.get('/:id/filters', getRecipeFilterByRecipeId);                  // GET /recipes/:id/filters
+router.post('/:id/filters', createRecipeFilter);                        // POST /recipes/:id/filters
+router.delete('/:id_recipe/filters/:id_filter', deleteRecipeFilter);    // DELETE /recipes/:id_recipe/filters/:id_filter
 
+router.get('/:id/picture', getRecipePicture);       // GET /recipes/:id/picture
+router.post('/:id/picture', createRecipePicture);   // POST /recipes/:id/picture
 
 module.exports = router;
