a2e29b3ceed9812d414271c19ac8b1aae46384a3
cnoah <noah.charrinbourrat@ynov.com>
2026-04-14 19:47:48 +0200

    refactor: remove unused route for fetching pictures by type

--- routes/routes_picture.js
+++ routes/routes_picture.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const {
   getAllPictures,
   getPictureById,
-  getPicturesByType,
   addPicture,
   updatePicture,
   deletePicture
@@ -11,7 +10,6 @@ const {
 
 router.get('/', getAllPictures);              // GET /pictures
 router.get('/:id', getPictureById);           // GET /pictures/:id
-router.get('/type/:type', getPicturesByType); // GET /pictures/type/:type
 router.post('/', addPicture);                 // POST /pictures
 router.put('/:id', updatePicture);            // PUT /pictures/:id
 router.delete('/:id', deletePicture);         // DELETE /pictures/:id
