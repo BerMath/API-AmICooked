01547aaf7a5fad90a5034925fc828cd340ae6389
cnoah <noah.charrinbourrat@ynov.com>
2026-04-17 19:51:23 +0200

    feat: add getUserRole function to retrieve user role from database

--- services/role.service.js
+++ services/role.service.js
@@ -1,11 +1,19 @@
 const {promisePool: db} = require('../config/database');
 
-async function hasRequiredRole({ userId, roleMin }) {
+async function getUserRole(userId) {
     const [user] = await db.query('SELECT role FROM Users WHERE id = ?', [userId]);
-    return user[0].role >= roleMin;
+    if (user.length === 0) {
+        return null
+    }
+    return user[0].role;
+}
+
+async function hasRequiredRole({userId, roleMin}) {
+    return await getUserRole(userId) >= roleMin;
 }
 
 module.exports = {
-    hasRequiredRole
+    hasRequiredRole,
+    getUserRole
 };
 
