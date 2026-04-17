ff56b23b6add227c7393ec10af925c35def7e34f
cnoah <noah.charrinbourrat@ynov.com>
2026-04-17 21:24:33 +0200

    feat: added middleware to check if the user is logged in when doing requests

--- routes/routes_recipes.js
+++ routes/routes_recipes.js
@@ -14,19 +14,22 @@ const {
     deleteRecipeFilter
 } = require('../controllers/controller_recipe_filters');
 const {getRecipePicture, handleRecipePicture} = require("../controllers/controller_picture");
+const {requireAuth, requireRoleOrSameUser} = require("../middleware/auth.middleware");
 
-router.get('/', getRecipes);            // GET /recipes
-router.get('/:id', getRecipeById);      // GET /recipes/:id
-router.post('/', createRecipe);         // POST /recipes
-router.post('/:id/time', timeRecipe);   // POST /recipes/:id/time
-router.put('/:id', updateRecipe);       // PUT /recipes/:id
-router.delete('/:id', deleteRecipe);    // DELETE /recipes/:id
+router.use(requireAuth);
 
-router.get('/:id/filters', getRecipeFilterByRecipeId);                  // GET /recipes/:id/filters
-router.post('/:id/filters', createRecipeFilter);                        // POST /recipes/:id/filters
-router.delete('/:id_recipe/filters/:id_filter', deleteRecipeFilter);    // DELETE /recipes/:id_recipe/filters/:id_filter
+router.get('/', getRecipes);                                                // GET /recipes
+router.get('/:id', getRecipeById);                                          // GET /recipes/:id
+router.post('/', createRecipe);                                             // POST /recipes
+router.post('/:id/time', timeRecipe);                                       // POST /recipes/:id/time
+router.put('/:id', updateRecipe);                                           // PUT /recipes/:id
+router.delete('/:id', deleteRecipe);                                        // DELETE /recipes/:id
 
-router.get('/:id/picture', getRecipePicture);       // GET /recipes/:id/picture
-router.post('/:id/picture', handleRecipePicture);   // POST /recipes/:id/picture
+router.get('/:id/filters', getRecipeFilterByRecipeId)                       // GET /recipes/:id/filters
+router.post('/:id/filters', createRecipeFilter);                            // POST /recipes/:id/filters
+router.delete('/:id_recipe/filters/:id_filter', deleteRecipeFilter);        // DELETE /recipes/:id_recipe/filters/:id_filter
+
+router.get('/:id/picture', getRecipePicture);                               // GET /recipes/:id/picture
+router.post('/:id/picture', handleRecipePicture);                           // POST /recipes/:id/picture
 
 module.exports = router;
